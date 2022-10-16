5b24c86c955ae4c017e8ce7a57f8c7a0a8240db2
clam2001 <108890680+clam2001@users.noreply.github.com>
2022-10-16 20:00:21 +0200

    Connle's CSS+HTML
    YESSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { AppointmentListComponent } from './components/appointment-list/appointment-list.component';
-import { AppointmentDetailsComponent } from './components/appointment-details/appointment-details.component'; 
+import { AppointmentDetailsComponent } from './components/appointment-details/appointment-details.component';
 
 import {
   HighchartsChartModule
 } from "highcharts-angular";
 
-  
+
 import { AdminLoginComponent } from './components/admin/admin-login/admin-login.component';
 import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
 import { AdminTabComponent } from './components/admin/admin-tab/admin-tab.component';
 
 import { SalesComponent } from './components/sales/sales.component';
 import { ListComponent } from './components/book-now/list/list.component';
-import { ProfileComponent } from './components/profile/profile.component'; 
-import { ReportTestComponent } from './components/report-test/report-test.component'; 
-import { SalonComponent } from './components/salon/salon.component'; 
-import { AddAppointmentComponent } from './components/add-appointment/add-appointment.component'; 
+import { ProfileComponent } from './components/profile/profile.component';
+import { ReportTestComponent } from './components/report-test/report-test.component';
+import { SalonComponent } from './components/salon/salon.component';
+import { AddAppointmentComponent } from './components/add-appointment/add-appointment.component';
 import { HomeComponent } from './components/home/home.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
@@ -45,10 +45,10 @@ const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
 const routes: Routes = [
   {
     path: '',
-    
+
     pathMatch: 'full',
     component: LoginComponent
-    
+
   },
   {
     path: 'landing',
@@ -68,13 +68,13 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
-    ...canActivate(redirectUnauthorizedToLogin),
+    //...canActivate(redirectUnauthorizedToLogin),
   },
   {
     path: 'add-appointment',
     component: AddAppointmentComponent,
    // ...canActivate(redirectUnauthorizedToLogin),
-   
+
   },
   {
     path: 'book-now',
@@ -104,29 +104,37 @@ const routes: Routes = [
   },
   { path: 'pokemon', loadChildren: () => import('./core/features/pokemon/pokemon.module').then(m => m.PokemonModule) },
 
-  
+
   {
     path: 'sales',
     component: SalesComponent,
-  }, 
+  },
   {
     path: 'admin-dashboard',
     component: AdminDashboardComponent,
+<<<<<<< Updated upstream
     canActivate: [AuthGuardAdminService]
   }, 
+=======
+  },
+>>>>>>> Stashed changes
   {
     path: 'admin-login',
     component: AdminLoginComponent,
-  }, 
+  },
   {
     path: 'admin',
     component: AdminTabComponent,
+<<<<<<< Updated upstream
     canActivate: [AuthGuardAdminService]
   }, 
   {
     path: 'booking-payment',
     component: AppBookingPaymentComponent
   }
+=======
+  },
+>>>>>>> Stashed changes
 
 ];
 
@@ -134,7 +142,7 @@ const routes: Routes = [
   declarations:[],
   imports: [
     CommonModule,
-  
+
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
